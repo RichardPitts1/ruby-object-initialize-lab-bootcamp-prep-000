0ededba6c548c5c339f33870307e3a12220b0d24
Richard Pitts <richardepitts@gmail.com>
2019-07-10 18:33:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,3 +16,22 @@ class Dog
     @breed
   end 
 end 
+
+class Dog   
+  def initialize(pups_name, pups_breed = "Mutt")  
+    @name = pups_name
+    @breed = pups_breed
+    
+  def name=(pups_name, pups_breed = "Mutt")
+    @name = pups_name
+    @breed = pups_breed
+    
+    def name
+      @name
+    end
+    
+    def breed
+      @breed
+    end
+    
+  end
